327e975e96cd8706ec55a910a067ef0a7c58c51a
poojithamiryala <poojithamiryala@gmail.com>
2020-06-18 09:41:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { IAngularMyDpOptions, IMyDateModel } from 'angular-mydatepicker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'piggy-bank';
+  openedSideNav = false;
+
+
+  myDpOptions: IAngularMyDpOptions = {
+    dateRange: false,
+    dateFormat: 'dd-mm-yyyy'
+    // other options are here...
+  };
+
+  from: IMyDateModel = null;
+  to: IMyDateModel = null;
+  fromDate: Date;
+  toDate: Date;
+
+  amount;
+  intrestRate;
+  // Initialized to specific date (09.10.2018).
+  openSideNav() {
+
+  }
+  onDateChanged(date, from?) {
+    console.log(date.singleDate.formatted)
+    if (from) {
+      this.from = date.singleDate.formatted;
+      this.fromDate = new Date(date.singleDate.formatted.replace(/(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3"));
+    } else {
+      this.to = date.singleDate.formatted;
+      this.toDate = new Date(date.singleDate.formatted.replace(/(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3"));
+    }
+  }
+
+  calculateIntreset() {
+    const timeDiff = this.toDate.getTime() - this.fromDate.getTime();
+    const days = timeDiff / (1000 * 60 * 60 * 24);
+    this.intrestRate = (this.amount) * 0.1 * (days/365);
+    this.intrestRate=this.intrestRate.toFixed(2);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { SatDatepickerModule, SatNativeDateModule } from 'saturn-datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { AngularMyDatePickerModule } from 'angular-mydatepicker';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    SatDatepickerModule,
+    SatNativeDateModule,
+    AngularMyDatePickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
